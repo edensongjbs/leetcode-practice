e951df93ebf19a68a879d8ad4f6b4deb9f790421
edensongjbs <edensongjbs@gmail.com>
2020-10-02 16:05:23 -0400

    add twosum

--- day1.js
+++ day1.js
@@ -72,3 +72,13 @@ var reverse = function(x) {
 //     return answer
 // }
 
+// twoSum 
+
+var twoSum = function(nums, target) {
+    for (let i1=0; i1 < nums.length; i1++) {
+        for (let i2=i1+1; i2 < nums.length; i2++)
+        if (nums[i1]+nums[i2]==target) {
+            return [i1, i2]
+        }
+    }
+};
