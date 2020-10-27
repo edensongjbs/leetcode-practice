5d31fb653cee82c6b1a1006637137c74e6c0f620
edensongjbs <edensongjbs@gmail.com>
2020-10-27 18:29:58 -0400

    remove element

--- independent-practice.js
+++ independent-practice.js
@@ -219,3 +219,21 @@ var isValid = function(s) {
     if (pars.length > 0) {return false}
     else {return true}
 };
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let i=0, j=1
+    while (j < nums.length) {
+        if (nums[j]!==val) {
+            nums[i]=nums[j]
+            i++
+        }
+        j++
+    }
+    return i
+};
