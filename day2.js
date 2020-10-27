09a6821217b8fb3a1d7d896bb466acc55e45ad5c
edensongjbs <edensongjbs@gmail.com>
2020-10-27 18:30:16 -0400

    after save

--- day2.js
+++ day2.js
@@ -31,14 +31,3 @@ var mergeTwoLists = function(l1, l2) {
     return l3head
 };
 
-const removeDuplicates = function(nums) {
-    if (nums.length === 0) return 0;
-    let i = 0;
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[j] != nums[i]) {
-            i++;
-            nums[i] = nums[j];
-        }
-    }
-    return i + 1;
-};
