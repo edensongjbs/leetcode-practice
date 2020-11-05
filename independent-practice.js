ca0471000e0cca47586d77eecaa372b49d9eee51
edensongjbs <edensongjbs@gmail.com>
2020-11-04 21:59:29 -0500

    complete isSameTree

--- independent-practice.js
+++ independent-practice.js
@@ -106,3 +106,13 @@ var longestPalindrome = function(s) {
         }
     }
 };
+
+//Good solution!
+var isSameTree = function(p, q) {
+    if (p===null && q===null){return true}
+    if (p===null || q===null){return false}
+    if (p.val!==q.val){return false}
+    else {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    }
+};
