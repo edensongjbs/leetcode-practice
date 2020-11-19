e3a5a6b84b877beaaa563c6e1956338e9f861785
edensongjbs <edensongjbs@gmail.com>
2020-11-19 13:17:35 -0500

    correct answer

--- pramp-practice.js
+++ pramp-practice.js
@@ -1,15 +1,22 @@
-function calcDroneMinEnergy(route) {
+// function calcDroneMinEnergy(route) {
+//     // your code goes here
+//     let usage = 0
+//     for (let i=1; i < route.length; i++) {
+//       let distance = (
+//           ((route[i][0]-route[i-1][0])**2) +
+//           ((route[i][1]-route[i-1][1])**2) +
+//           ((route[i][2]-route[i-1][2])**2)
+//         )**0.5
+//       usage = usage + ((route[i][2] <= route[i-1][2]) ? distance : 0-distance)
+//     }
+//     console.log((usage))
+//   }
+
+  function calcDroneMinEnergy(route) {
     // your code goes here
-    let usage = 0
-    for (let i=1; i < route.length; i++) {
-      let distance = (
-          ((route[i][0]-route[i-1][0])**2) +
-          ((route[i][1]-route[i-1][1])**2) +
-          ((route[i][2]-route[i-1][2])**2)
-        )**0.5
-      usage = usage + ((route[i][2] <= route[i-1][2]) ? distance : 0-distance)
-    }
-    console.log((usage))
+    let justZs = route.map(e => e[2])
+    let maxHeight = Math.max(...justZs)
+    return maxHeight-justZs[0]
   }
 
   calcDroneMinEnergy([ [0,   2, 10],
