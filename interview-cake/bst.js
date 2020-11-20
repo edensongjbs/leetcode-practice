0b298478e05795ecb1b3244cfbdd0734d5a42cd6
edensongjbs <edensongjbs@gmail.com>
2020-11-20 13:22:14 -0500

    add interview cake solution

--- interview-cake/bst.js
+++ interview-cake/bst.js
@@ -118,5 +118,52 @@ const isBST = (root, lower, upper) => {
     }
 }
 
+// Interview Cake Iterative Solution
+// function isBinarySearchTree(treeRoot) {
+
+//     // Start at the root, with an arbitrarily low lower bound
+//     // and an arbitrarily high upper bound
+//     const nodeAndBoundsStack = [];
+//     nodeAndBoundsStack.push({
+//       node: treeRoot,
+//       lowerBound: Number.NEGATIVE_INFINITY,
+//       upperBound: Number.POSITIVE_INFINITY,
+//     });
+  
+//     // Depth-first traversal
+//     while (nodeAndBoundsStack.length) {
+//       const { node, lowerBound, upperBound } = nodeAndBoundsStack.pop();
+  
+//       // If this node is invalid, we return false right away
+//       if (node.value <= lowerBound || node.value >= upperBound) {
+//         return false;
+//       }
+  
+//       if (node.left) {
+  
+//         // This node must be less than the current node
+//         nodeAndBoundsStack.push({
+//           node: node.left,
+//           lowerBound,
+//           upperBound: node.value,
+//         });
+//       }
+  
+//       if (node.right) {
+  
+//         // This node must be greater than the current node
+//         nodeAndBoundsStack.push({
+//           node: node.right,
+//           lowerBound: node.value,
+//           upperBound,
+//         });
+//       }
+//     }
+  
+//     // If none of the nodes were invalid, return true
+//     // (At this point we have checked all nodes)
+//     return true;
+//   }
+
 module.exports = { isSuperBalanced, isBST, BinaryTreeNode }
 
