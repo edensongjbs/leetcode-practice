18f7f6d1b26be548d8e083257154d6a17ffb5392
edensongjbs <edensongjbs@gmail.com>
2020-11-23 14:22:53 -0500

    find duplicate without modifying input

--- interview-cake/graphs.js
+++ interview-cake/graphs.js
@@ -59,6 +59,36 @@ const getPath = (graph, startNode, endNode) => {
     return i;
   }
 
+  function findDuplicate(intArray) {
+
+    // Find a number that appears more than once ... in O(n) time
+    let i = intArray[intArray.length-1]
+    console.log(intArray)
+    for (let ct=0; ct<intArray.length; ct++) {
+      i=intArray[i-1]
+    }
+    console.log('inside loop', i)
+    let knownLoop = i, ct2=1
+    while (intArray[i-1]!==knownLoop) {
+      console.log(intArray[i-1])
+      i = intArray[i-1]
+      ct2++
+    }
+    console.log('loop length', ct2)
+    let farPointer = intArray[intArray.length-1], closePointer = intArray[intArray.length-1]
+    for (let ct3=0; ct3<ct2; ct3++) {
+      console.log(farPointer)
+      farPointer = intArray[farPointer-1]
+    }
+    console.log('farPointer assigned:', farPointer)
+    while (closePointer!==farPointer) {
+      closePointer = intArray[closePointer-1]
+      farPointer = intArray[farPointer-1]
+    }
+    return farPointer;
+  }
   
+
+
 
   module.exports = { getPath, Queue }
