ebfbfbc3917204074318211b145f9cc80b6e46e2
edensongjbs <edensongjbs@gmail.com>
2020-11-24 21:50:53 -0500

    kth to end linked list with notes

--- interview-cake/linked-list.js
+++ interview-cake/linked-list.js
@@ -49,3 +49,41 @@ function deleteNode(nodeToDelete) {
     return currentNode
   }
   
+  function kthToLastNode(k, head) {
+    let ct=1
+      
+    if (k < 1) {throw new Error}
+    if (!head){throw new Error}
+    let node=head
+    while (node.next) {
+      ct++
+      node=node.next
+    }
+    ct-=(k-1)
+    if (ct < 1){throw new Error} 
+    // Return the kth to last node in the linked list
+    node = head
+    while (ct > 1){
+      node=node.next
+      ct--
+    }
+    
+    return node;
+  }
+  
+  /* inputs: a int value and a list head node
+  outputs: return a node (kth from end)
+  
+  
+  example:2,  'Angel Food'-'Bundt'-'Cheese'-"Devil's Food"-'Eccles'
+  exp: Devil's Food
+  
+  
+  constraints:
+  
+  edge cases: empty list, k < 0, k larger than length of list
+  
+  time complexity: O(n)
+  
+  
+  */
