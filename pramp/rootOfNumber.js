2fb57cdfe6d8e0b44692e9a90a7baa38a5d884f8
edensongjbs <edensongjbs@gmail.com>
2021-01-08 16:18:28 -0500

    working solution, renamed variables

--- pramp/rootOfNumber.js
+++ pramp/rootOfNumber.js
@@ -1,15 +1,16 @@
 function root(x, n) {
     // your code goes here
-    let lower=0, upper=x, guess = (lower+upper)/2
+    let lower=0.0, upper=x, guess = (lower+upper)/2
     while ((guess-lower) >= 0.001) { //figure out condition
+    //   console.log(upper, lower, guess)
       if (Math.pow(guess, n)=== x) return guess
       else if ((Math.pow(guess, n)) > x) {
       // bring down upperbound
-        upperbound = guess //4.5
+        upper = guess //4.5
       }
       else {
-        //bring down lowerbound
-        lowerbound = guess
+        //bring up lowerbound
+        lower = guess
       }
       guess = (lower+upper)/2
     }
